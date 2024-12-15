b3892bb0bd9b7f2e51661d0a69afd11ca9bb93a4
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-15 07:47:08 -0500

    Update README.md

---
+++ o-o.js
@@ -0,0 +1,40 @@
+// @ts-check
+import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
+import { emc as baseEMC } from './emc.js';
+import {Registry} from 'be-hive/Registry.js';
+import {aggs} from 'be-hive/aggEvt.js';
+import { w as bw } from 'be-hive/w.js';
+/** @import  {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types' */;
+/** @import {CSSQuery} from './ts-refs/trans-render/types.js' */
+
+export const emc = {
+    ...baseEMC,
+    base: 'o-o',
+    enhPropKey: 'OO',
+    handlerKey: 'OO',
+    ws: []
+};
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose);
+
+for(const key in aggs){
+    Registry.register(emc, key, aggs[key]);
+}
+
+/**
+ * 
+ * @param {string} handlerName 
+ * @param {EventListenerOrFn} handler 
+ */
+export function register(handlerName, handler){
+    Registry.register(emc, handlerName, handler);
+}
+
+/**
+ * 
+ * @param {CSSQuery} q 
+ */
+export function w(q){
+    return bw(q, emc.ws);
+}
+
