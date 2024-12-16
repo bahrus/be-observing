3140e5617b716da1ef818cc37f4a3425c485a8c8
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-16 02:20:36 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-observing into baseline

--- tests/Mapping/ByName/boolishText.spec.mjs
+++ tests/Mapping/ByName/boolishText.spec.mjs
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 test('boolishText', async ({ page }) => {
     await page.goto('./tests/Mapping/ByName/boolishText.html');
     // wait for 1 second
-    await page.waitForTimeout(1500);
+    await page.waitForTimeout(2000);
     const editor = page.locator('#target');
     await expect(editor).toHaveAttribute('mark', 'good');
 });
