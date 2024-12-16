773c9022e1d3d44e4b0d07d6cb0dba42caa4710f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-15 20:47:48 -0500

    0.0.1

--- tests/FullInference/QuintessentialExample.spec.mjs
+++ tests/FullInference/QuintessentialExample.spec.mjs
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 test('QuintessentialExample', async ({ page }) => {
-    await page.goto('./tests/QuintessentialExample.html');
+    await page.goto('./tests/FullInference/QuintessentialExample.html');
     // wait for 1 second
     await page.waitForTimeout(2000);
     const editor = page.locator('#target');
