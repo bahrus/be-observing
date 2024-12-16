d65bf21201a5e54825851d2b9c0303bccb717452
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-16 02:23:36 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-observing into baseline

--- tests/FullInference/HostToID/input.spec.mjs
+++ tests/FullInference/HostToID/input.spec.mjs
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 test('FullInerence.HostToID.Input', async ({ page }) => {
     await page.goto('./tests/FullInference/HostToID/input.html');
     // wait for 1 second
-    await page.waitForTimeout(1500);
+    await page.waitForTimeout(2000);
     const editor = page.locator('#target');
     await expect(editor).toHaveAttribute('mark', 'good');
 });
