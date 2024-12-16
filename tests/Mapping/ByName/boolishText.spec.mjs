9227342071e9c9b997b0d6aa9dfa8144d63e2a8a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-15 21:07:15 -0500

    0.0.1

--- tests/Mapping/ByName/boolishText.spec.mjs
+++ tests/Mapping/ByName/boolishText.spec.mjs
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
-test('QuintessentialExample', async ({ page }) => {
-    await page.goto('./tests/FullInference/QuintessentialExample.html');
+test('boolishText', async ({ page }) => {
+    await page.goto('./tests/Mapping/ByName/boolishText.html');
     // wait for 1 second
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1500);
     const editor = page.locator('#target');
     await expect(editor).toHaveAttribute('mark', 'good');
 });
